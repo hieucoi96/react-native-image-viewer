db2d08c530cd9065c272816af54c5d2e5a032916
Hoang Minh Hieu <hieucoi@MacBook-Pro-cua-Hoang-2.local>
2022-05-26 10:42:56 +0700

    t2

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -548,8 +548,8 @@ export default class ImageViewer extends React.Component<Props, State> {
               onLongPress={this.handleLongPressWithIndex.get(index)}
               onClick={this.handleClick}
               onDoubleClick={this.handleDoubleClick}
-              imageWidth={width}
-              imageHeight={height}
+              imageWidth={screenWidth}
+              imageHeight={screenHeight}
               enableSwipeDown={this.props.enableSwipeDown}
               swipeDownThreshold={this.props.swipeDownThreshold}
               onSwipeDown={this.handleSwipeDown}
